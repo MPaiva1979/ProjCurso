91b8e387c160978376c396fd0a1d37d18c4702da
MPaiva1979 <marcosrodriguespaiva@gmail.com>
2024-05-20 22:31:54 -0300

    First steps in Java with Spring Boot 3

---
+++ 01_ProjCurso/ProjCurso/src/main/java/br/com/paiva/ProjCursoApplication.java
@@ -0,0 +1,13 @@
+package br.com.paiva;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProjCursoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProjCursoApplication.class, args);
+	}
+
+}
---
+++ 01_ProjCurso/ProjCurso/src/test/java/br/com/paiva/ProjCursoApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.paiva;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProjCursoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
